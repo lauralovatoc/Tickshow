4a25c53e250c2636ab13c0f9d2d67c313e74bb7b
Laura Lovato <103641723+lauralovatoc@users.noreply.github.com>
2023-10-23 10:41:10 -0300

    js e css

--- projetoTickshow/index.php
+++ projetoTickshow/index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Tickshow!</title>
+        
+        <link href="css/maxcdn.bootstrapcdn.com_bootstrap_3.4.1_css_bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/ajax.googleapis.com_ajax_libs_jquery_3.6.4_jquery.min.js" type="text/javascript"></script>
+        <script src="js/maxcdn.bootstrapcdn.com_bootstrap_3.4.1_js_bootstrap.min.js" type="text/javascript"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
     </body>
 </html>
